8dce54167ad1a576272c53bd2ab6a5e0b845f200
mswift42 <martin.haesler@gmail.com>
2015-09-21 15:30:15 +0100

    Add faces variable.

--- themecreator.go
+++ themecreator.go
@@ -1,1 +1,9 @@
 package itc
+
+// Themeface represents the name of a color theme variable,
+// for example keyword, variable, background, ... .
+type Themeface string
+
+var faces = []Themeface{"mainbg", "mainfg", "keyword",
+	"comment", "type", "variable", "functionname",
+	"warning", "doc", "string"}
